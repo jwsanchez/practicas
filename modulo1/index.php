e7eb45ecbb6ed3451914045edc3cc8f46ee02e98
Jesus Sanchez <jwsanchezj96@gmail.com>
2020-10-07 17:25:28 -0500

    Commit inicial para practicas basicas en Front

---
+++ modulo1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti delectus error iste laboriosam. Consectetur
+        doloribus libero incidunt harum, numquam odio assumenda natus minima qui quasi sit ratione quas! Voluptate,
+        provident.</p>
+    <h1><?php echo "Hola mundo" ?></h1>
+</body>
+
+</html>
